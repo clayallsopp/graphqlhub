05731575ad81429b99be69700bb672a2bd8a0ba1
Julien Viala <dev.vialajulien@gmail.com>
2016-01-14 18:08:50 +0100

    get a promise from factory

--- apis/twitter.js
+++ apis/twitter.js
@@ -1,4 +1,5 @@
 import Twit from 'Twit';
+import getObjProperty from 'lodash.get';
 
 const {
     TWITTER_ACCESS_SECRET,
@@ -14,95 +15,28 @@ const Twitter = new Twit({
     access_token_secret: TWITTER_ACCESS_SECRET
 });
 
-export const getUser = (authKey, authValue) => {
+export const getUser = (authKey, authValue) =>  __getPromise('users/show', { [authKey]: authValue });
+export const getTweets = (user_id, count) =>    __getPromise('statuses/user_timeline', { user_id, count });
+export const getTweet = (id) =>                 __getPromise('statuses/show', { id });
+export const getRetweets = (id, count) =>       __getPromise('statuses/retweets', { id, count });
+export const searchFor = (queryArgs) =>         __getPromise("search/tweets", queryArgs, 'statuses');
+
+const __getPromise = (endpoint, args, resultPath = null) => {
 
     return new Promise((resolve, reject) => {
 
         Twitter.get(
-            'users/show',
-            {
-                [authKey]: authValue
-            },
-            (error, user) => {
+            endpoint,
+            args,
+            (error, result) => {
 
                 if (error) {
                     reject(error);
                 }
                 else {
-                    resolve(user);
+                    resolve( resultPath !== null ? getObjProperty(result, resultPath) : result );
                 }
             }
         )
     });
 };
-
-export const getTweets = (userId, limit) => {
-
-    return new Promise((resolve, reject) => {
-
-        Twitter.get(
-            'statuses/user_timeline',
-            {
-                user_id: userId,
-                count: limit
-            },
-            (error, tweets) => {
-
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    resolve(tweets);
-                }
-            }
-        )
-    });
-};
-
-export const getTweet = (tweetId) => {
-
-    return new Promise((resolve, reject) => {
-
-        Twitter.get(
-            'statuses/show',
-            {
-                id: tweetId
-            },
-            (error, tweet) => {
-
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    resolve(tweet);
-                }
-            }
-        )
-    });
-};
-
-export const getRetweets = (tweetId, limit) => {
-
-    return new Promise((resolve, reject) => {
-
-        Twitter.get(
-            'statuses/retweets',
-            {
-                id: tweetId,
-                count: limit
-            },
-            (error, retweets) => {
-
-                if (error) {
-                    reject(error);
-                }
-                else {
-                    resolve(retweets);
-                }
-            }
-        )
-    });
-};
-
-
-//function generateAPIPromise(url, )
