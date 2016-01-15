8d512beb6dfe73ce4c4782fca8cb78a3565da7dc
Julien Viala <dev.vialajulien@gmail.com>
2016-01-15 23:33:39 +0100

    use appropriate capitalization

--- schemas/graphqlhub.js
+++ schemas/graphqlhub.js
@@ -9,14 +9,14 @@ import { Schema as HN } from './hn';
 import { Schema as REDDIT } from './reddit';
 import { Schema as KEYVALUE } from './keyvalue';
 import { Schema as GITHUB } from './github';
-import { Schema as Twitter } from './twitter';
+import { Schema as TWITTER } from './twitter';
 
 let schemas = {
   hn : HN,
   reddit : REDDIT,
   keyValue : KEYVALUE,
   github : GITHUB,
-  twitter: Twitter
+  twitter: TWITTER
 };
 
 let FIELDS = {
