6d0da096f0f337c462c1bc547b9a44a035e4108a
Julien Viala <dev.vialajulien@gmail.com>
2016-01-17 18:55:32 +0100

    more key/colon/value spacing

--- apis/twitter.js
+++ apis/twitter.js
@@ -9,17 +9,17 @@ const {
 } = process.env;
 
 const Twitter = new Twit({
-  consumer_key: TWITTER_CONSUMER_KEY,
-  consumer_secret: TWITTER_CONSUMER_SECRET,
-  access_token: TWITTER_ACCESS_TOKEN,
-  access_token_secret: TWITTER_ACCESS_SECRET
+  consumer_key        : TWITTER_CONSUMER_KEY,
+  consumer_secret     : TWITTER_CONSUMER_SECRET,
+  access_token        : TWITTER_ACCESS_TOKEN,
+  access_token_secret : TWITTER_ACCESS_SECRET
 });
 
-export const getUser = (authKey, authValue) =>  __getPromise('users/show', { [authKey]: authValue });
-export const getTweets = (user_id, count) =>    __getPromise('statuses/user_timeline', { user_id, count });
-export const getTweet = (id) =>                 __getPromise('statuses/show', { id });
-export const getRetweets = (id, count) =>       __getPromise('statuses/retweets', { id, count });
-export const searchFor = (queryParams) =>       __getPromise("search/tweets", queryParams, 'statuses');
+export const getUser = (authKey, authValue) => __getPromise('users/show', { [authKey]: authValue });
+export const getTweets = (user_id, count)   => __getPromise('statuses/user_timeline', { user_id, count });
+export const getTweet = (id)                => __getPromise('statuses/show', { id });
+export const getRetweets = (id, count)      => __getPromise('statuses/retweets', { id, count });
+export const searchFor = (queryParams)      => __getPromise("search/tweets", queryParams, 'statuses');
 
 const __getPromise = (endpoint, parameters, resultPath = null) => {
 
