c7c284c7494d5336c88cc7f1d37194c136e03ab9
Clay Allsopp <clay.allsopp@gmail.com>
2016-01-25 07:59:55 -0800

    change fieldType to be one property

--- src/timingCallback.js
+++ src/timingCallback.js
@@ -3,9 +3,10 @@ import keen from './keen';
 export default (timings) => {
   keen.addEvent('totalDurations', { duration : timings.duration });
   timings.fields.forEach(({ type, field, args, duration }) => {
+    let fieldType = `${type}#${field}`;
     // filter out really fast things (probably just obj.property calls)
     if (duration > 0.005) {
-      keen.addEvent('fieldDurations', { type, field, duration });
+      keen.addEvent('fieldDurations', { type, field, duration, fieldType });
     }
   });
 }
